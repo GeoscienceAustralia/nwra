194eadc34743ee8ae4a36f0738a5ad7bfa8c9b1f
wcarthur <wcarthur@gmail.com>
2023-12-04 13:03:53 +1100

    Batch scripts to run HazImp

---
+++ run_hazimp.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Run HazImp in batch mode
+
+# This shell script is not intended to be run directly. It is called by
+# another script `run_hazimp_batch.sh`, which is designed to spawn a number of
+# shell scripts in an MPI environment. See the comments in `run_hazimp_batch.sh`
+#
+# Craig Arthur
+# 2023-11-23
+
+
+module purge
+module load pbs
+module load dot
+
+module load python3/3.7.4
+module load netcdf/4.6.3
+module load hdf5/1.10.5
+module load geos/3.8.0
+module load proj/6.2.1
+module load gdal/3.0.2
+module load openmpi/4.0.1
+
+# Need to ensure we get the correct paths to access the local version of gdal bindings.
+# The module versions are compiled against Python3.6, so we can't use them
+export PYTHONPATH=/g/data/w85/.local/lib/python3.7/site-packages:$PYTHONPATH
+
+# Add the local Python-based scripts to the path:
+export PATH=/g/data/w85/.local/bin:$PATH
+
+# Needs to be resolved, but this suppresses an error related to HDF5 libs
+export HDF5_DISABLE_VERSION_CHECK=2
+
+SOFTWARE=/g/data/w85/software
+
+# Add HazImp code to the path:
+export PYTHONPATH=$SOFTWARE/hazimp:$PYTHONPATH
+
+cd $SOFTWARE/hazimp/
+
+DATE=`date +%Y%m%d%H%M`
+CONFIGDIR=/g/data/w85/software/nwra
+OUTPUTDIR=/scratch/w85/nwra/impact/$STATE
+
+if [ ! -d $OUTPUTDIR ]; then
+    mkdir -p --mode 775 $OUTPUTDIR
+fi
+
+if [ $# -eq 0 ]; then
+    # Use an environment variable
+    RP=$RP
+else
+    RP=$1
+fi
+
+if [ $# -eq 0 ]; then
+    # Use environment variable
+    STATE=$STATE
+else
+    STATE=$2
+fi
+
+OUTPUTDIR=/scratch/w85/nwra/impact/$STATE
+
+if [ ! -d $OUTPUTDIR ]; then
+    mkdir -p --mode 775 $OUTPUTDIR
+fi
+echo "Running hazimp for $STATE and $RP"
+
+CONFIGFILE=$CONFIGDIR/$RP.$STATE.yaml
+OUTPUT=/scratch/w85/nwra/impact
+
+# Substitute the paths into the template configuration file:
+sed 's|RPX|'$RP'|g' /g/data/w85/software/nwra/hazimp_template.yaml > $CONFIGFILE
+sed -i 's|STATE|'$STATE'|g' $CONFIGFILE
+chmod g+w $CONFIGFILE
+
+if [ ! -f "$CONFIGFILE" ]; then
+    echo "Configuration file does not exist: $CONFIGFILE"
+    exit 1
+else
+    echo $CONFIGFILE
+fi
+
+python3 $SOFTWARE/hazimp/hazimp/main.py -c $CONFIGFILE 
+
+# Clean up to reduce inode usage
+rm -rf $OUTPUTDIR/$RP.$STATE.png
+#rm -rf $OUTPUTDIR/$RP.$STATE.xml
+#rm -rf $CONFIGFILE
---
+++ run_hazimp_batch.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#PBS -Pw85
+#PBS -qhugemem
+#PBS -N hazimpbatch
+#PBS -m ae
+#PBS -M craig.arthur@ga.gov.au
+#PBS -lwalltime=6:00:00
+#PBS -lmem=1024GB,ncpus=32,jobfs=4000MB
+#PBS -joe
+#PBS -W umask=0002
+#PBS -lstorage=gdata/w85+scratch/w85
+ 
+# Load module, always specify version number.
+module load nci-parallel/1.0.0a
+
+# This job script spawns a bunch of new shells to run multiple scripts in
+# parallel. As this is the parent job, you must request sufficient resources
+# (i.e. memory and job file storage) to ensure *all* processes can run
+# concurrently. 
+
+# The actual script to run (and any required command line arguments) are
+# specified in the companion "cmd.txt" file - it is passed to the nci-parallel
+# program using the --input-file option below
+ 
+export ncores_per_task=1
+export ncores_per_node=48
+ 
+# Must include `#PBS -l storage=scratch/w85+gdata/hh5` if the job
+# needs access to `/scratch/w85/` and `/g/data/hh5/`. Details on:
+# https://opus.nci.org.au/display/Help/PBS+Directives+Explained
+
+pattern="(RP[0-9]{1,5})_smooth$"
+SOURCEDIR=/scratch/w85/nwra/hazard/local
+SCRIPTDIR=/g/data/w85/software/nwra
+STATE="QLD"
+# Remove any existing cmd.txt file:
+[[ -f $SCRIPTDIR/cmd.txt ]] && rm -rf $SCRIPTDIR/cmd.txt
+
+# Add a command for each local wind field:
+for dir in "$SOURCEDIR"/*; do
+    echo $dir
+    if [[ $dir =~ $pattern ]]; then
+        RP="${BASH_REMATCH[1]}"
+
+        echo "/g/data/w85/software/nwra/run_hazimp.sh $RP $STATE" >> $SCRIPTDIR/cmd.txt
+    fi
+done
+
+#mpirun -np $((PBS_NCPUS/ncores_per_task))  --map-by ppr:$((ncores_per_node/ncores_per_task)):NODE:PE=${ncores_per_task}:OVERSUBSCRIBE nci-parallel --input-file $SCRIPTDIR/cmd.txt --timeout 4000
